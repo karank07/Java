a12bae0bbe64486c79c42ce7faffb53d419ea099
Karan Keswani <37830740+karank07@users.noreply.github.com>
2019-09-30 00:08:14 -0400

    Update armstrong.java

--- Practice Programs/src/practice/programs/armstrong.java
+++ Practice Programs/src/practice/programs/armstrong.java
@@ -1,11 +1,5 @@
 package practice.programs;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Karan
