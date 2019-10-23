bd04f8de3aae1db16ba9d3fbf646898cbdab11a6
Karan <karankeswani.79@gmail.com>
2019-10-23 14:42:23 -0400

    updated with socket.close

--- Practice Programs/src/sockets/BroadcasterGUI/BServer.java
+++ Practice Programs/src/sockets/BroadcasterGUI/BServer.java
@@ -5,24 +5,37 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
+/**
+ * @author Karan
+ *
+ */
+
+/**
+ * 
+ * class BServer to create server socket and maintain list of socket connections of server
+ */
 public class BServer {
 
 	ServerSocket ss;
-	ArrayList<ServerHandler> clients = new ArrayList<ServerHandler>();
-
+	ArrayList<ServerHandler> socketsList = new ArrayList<ServerHandler>();
+	ServerHandler sh;
+	
 	public static void main(String[] args) throws IOException {
 		new BServer();
 	}
 
+	/**
+	 * Constructor to create server socket and initialize ServerHandler object, 
+	 */
 	public BServer() {
 		try {
 			ss = new ServerSocket(9996);
 			while (true) {
 				Socket s = ss.accept();
 				System.out.println("New Client connected: " + s);
-				ServerHandler sh = new ServerHandler(s, this);
+				sh= new ServerHandler(s, this);
 				sh.start();
-				clients.add(sh);
+				socketsList.add(sh);
 			}
 
 		} catch (IOException e) {
